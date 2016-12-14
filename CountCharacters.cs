3a1347822ec48196d6a7fe6081a4b7210ee9dccf
Luke Burgess-Yeo <luke.burgess94@gmail.com>
2016-12-14 17:23:02 +0000

    Counting unique characters.
    Function which takes a string and counts how many of each character there is.

---
+++ CountCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            countCharacters("Hello My Name Is Luke.");
+            Console.Read();
+        }
+
+        static void countCharacters(string input)
+        {
+            char[] unique = input.Distinct().ToArray();
+            Array.Sort(unique);
+            char[] all = input.ToArray();
+
+            foreach (char u in unique)
+            {
+                int count = 0;
+                foreach (char a in all)
+                    if (u == a) count++;
+
+                Console.WriteLine(u + " : " + count);
+            }
+        }
+    }
+}
